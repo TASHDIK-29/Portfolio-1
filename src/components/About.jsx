e805436bc2d604f1ef3556d752110a73dd28f165
Tashdik <s2110477153@ru.ac.bd>
2024-07-03 03:16:01 +0600

    responsive on processing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import React, { useEffect } from 'react';
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import Image from 'next/image';
+import Link from 'next/link';
 
 const About = () => {
 
@@ -13,26 +14,28 @@ const About = () => {
     }, [])
 
     return (
-        <div className='min-h-[calc(100vh-120px)] flex '>
-            <div className='w-1/2 p-16 flex items-center justify-center'>
-                <div className='bg-green-400 w-1/2 h-[300px] relative' data-aos="fade-left">
+        <div className='min-h-[calc(100vh-120px)] md:flex p-4'>
+            <div className='md:w-1/2 p-8 md:p-16 flex items-center justify-center'>
+                <div className='bg-green-400 w-full md:w-1/2 h-[300px] relative' data-aos="fade-left">
                     <div className='h-full w-full absolute left-4 bottom-4' >
                         {/* <img className='h-full' src="/images/about.png" alt="img" data-aos="flip-up"/> */}
                         <Image className='h-full' src="/images/about.png" alt='about.png' width={495} height={505}></Image>
                     </div>
                 </div>
             </div>
-            <div className='w-1/2 flex flex-col justify-center items-start' data-aos="fade-left">
-                <div className='flex flex-col p-10'>
-                    <h1 className='text-3xl font-bold mb-10 flex items-center gap-10'>About ME <span className='w-56 h-0 border-2 border-dashed border-green-400'></span></h1>
+            <div className='w-full md:w-1/2 flex flex-col justify-center items-start' data-aos="fade-left">
+                <div className='flex flex-col md:p-10'>
+                    <h1 className='text-3xl font-bold my-10 flex items-center gap-10 w-4/5 md:w-full mx-auto'>About ME <span className='flex-1 md:w-56 h-0 border-2 border-dashed border-green-400'></span></h1>
                     <h3>Motivated Junior MERN stack Developer with a passion for developing responsive, user-friendly web applications. Proficient in core web development technologies. Quick learner and enthusiastic Junior MERN stack Developer seeking to contribute to a team environment.</h3>
 
                 </div>
 
-                <div className='p-10'>
-                    <a className='px-4 py-2 mr-5 bg-green-500 text-slate-950 font-semibold' href="https://drive.google.com/file/d/1zMITfM6hdi392VHGHWyRObdfT6sUYObu/view" target="_blank">Resume</a>
-                    <a className='px-4 py-2 mr-5 bg-green-500 text-slate-950 font-semibold' href="https://www.linkedin.com/in/tashdikurrahmankhan" target="_blank">Linkedin</a>
-                    <a className='px-4 py-2 mr-5 bg-green-500 text-slate-950 font-semibold' href="https://github.com/TASHDIK-29" target="_blank">Github</a>
+                <div className='py-4 md:p-10  w-full'>
+                    <a className=' px-2 md:px-4 py-2 mr-2 md:mr-5 bg-green-500 text-slate-950 font-semibold' href="./TashdikResume.pdf" target="_blank">Resume</a>
+                    <a className=' px-2 md:px-4 py-2 mr-2 md:mr-5 bg-green-500 text-slate-950 font-semibold' download href="./TashdikResume.pdf">Resume</a>
+                    {/* <Link target="_blank" href="./Resume-F.pdf">Download Resume</Link> */}
+                    <a className=' px-2 md:px-4 py-2 mr-2 md:mr-5 bg-green-500 text-slate-950 font-semibold' href="https://www.linkedin.com/in/tashdikurrahmankhan" target="_blank">Linkedin</a>
+                    <a className=' px-2 md:px-4 py-2 mr-2 md:mr-5 bg-green-500 text-slate-950 font-semibold' href="https://github.com/TASHDIK-29" target="_blank">Github</a>
                 </div>
             </div>
         </div>
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -15,11 +15,11 @@ const Banner = () => {
     }, [])
 
     return (
-        <div className='min-h-[calc(100vh-64px)] flex'>
-            <div className='w-2/3  flex flex-col justify-center items-center' data-aos="fade-up">
+        <div className='min-h-[calc(100vh-64px)] flex flex-col-reverse md:flex-row p-8'>
+            <div className='mt-4 md:mt-0 md:w-3/5  flex flex-col justify-center items-center' data-aos="fade-up">
                 <div className=' space-y-6 '>
                     <h3 className='text-lg text-green-400'>Hello,</h3>
-                    <h1 className='text-5xl font-bold'>I&apos;m Tashdikur <br /> Rahman Khan</h1>
+                    <h1 className='text-3xl md:text-5xl font-bold'>I&apos;m Tashdikur <br /> Rahman Khan</h1>
                     <div className='flex'>
                         <span className='border-2 border-green-400 p-3 font-semibold'>WEB DEVELOPER</span>
                         <span className='bg-green-400 p-3 flex items-center'>
@@ -27,7 +27,7 @@ const Banner = () => {
                     </div>
 
                 </div>
-                <div className='w-2/6 mt-2'>
+                <div className='w-3/4 md:w-5/12 mt-2'>
                     <Marquee>
                         <span className='mx-4'>React.js</span>
                         <span className='mx-4'>Next.js</span>
@@ -41,10 +41,12 @@ const Banner = () => {
                     </Marquee>
                 </div>
             </div>
-            <div className='bg-green-600 w-1/3 relative'>
-                <div className=' absolute -left-72 bottom-0' data-aos="zoom-in-up">
-                    <Image className='h-[600px]' src="/images/new2.png" alt="image" width={840} height={729} />
-                    {/* <img className='h-[600px]' src="/images/new2.png" alt="image" /> */}
+            <div className='md:w-2/5 flex items-center justify-start'>
+                <div className='bg-green-400 w-full md:w-1/2 h-[300px] relative' data-aos="fade-left">
+                    <div className='h-full w-full absolute left-4 bottom-4' >
+                        {/* <img className='h-full' src="/images/about.png" alt="img" data-aos="flip-up"/> */}
+                        <Image className='h-full' src="/images/about.png" alt='about.png' width={495} height={505}></Image>
+                    </div>
                 </div>
             </div>
         </div>

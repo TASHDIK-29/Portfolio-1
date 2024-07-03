2797c6c32f5b6755ca0f79747f26a14677108bec
Tashdik <s2110477153@ru.ac.bd>
2024-07-03 12:53:43 +0600

    successfully sending email by email js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar></Navbar>
         <div className="min-h-[calc(100vh-64px)] bg-slate-950">{children}</div>
+        <Footer></Footer>
       </body>
     </html>
   );
